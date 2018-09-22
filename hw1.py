b8772bdf4c34a28f9b061db0373e31849425271a
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-09-22 14:36:43 -0700

    Finished consolidation, fixed cosine distances.

--- hw1.py
+++ hw1.py
@@ -13,14 +13,74 @@ import pandas as pd
 
 from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics.pairwise import cosine_distances
 
 import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
 
-def k_iter(X, y, folds=None, metric='euclidean'):
+def inverse_dist(distances):
+    """Inverse distance weighting function.
+
+    Weighting function that uses the inverse distance to weight each neighbor
+    in kNN classification.
+
+    Parameters
+    ----------
+    distances : array_like
+        The distance each neighbor is from the test point
+
+    Returns
+    -------
+    array_like
+        The weight to use for each neighbor.
+
+    """
+    return 1. / distances
+
+
+def square_inverse_dist(distances):
+    """Square inverse distance weighting function.
+
+    Weighting function that uses the square inverse distance to weight each
+    neighbor in kNN classification.
+
+    Parameters
+    ----------
+    distances : array_like
+        The distance each neighbor is from the test point
+
+    Returns
+    -------
+    array_like
+        The weight to use for each neighbor.
+
+    """
+    return 1. / (distances**2)
+
+
+def linear_dist(distances):
+    """Linear distance weighting function.
+
+    Weighting function that uses a linear function to weight each neighbor in
+    kNN classification.
+
+    Parameters
+    ----------
+    distances : array_like
+        The distance each neighbor is from the test point
+
+    Returns
+    -------
+    array_like
+        The weight to use for each neighbor.
+
+    """
+    return (np.max(distances) - distances) / \
+           (np.max(distances) - np.min(distances))
+
+
+def k_iter(X, y, key, folds=None, metric='euclidean', weights='uniform'):
     """k-neighbors iterator.
 
     Iterates through [1:k] neighbors for a kNN classifier, allowing for
@@ -33,8 +93,13 @@ def k_iter(X, y, folds=None, metric='euclidean'):
     y : array_like or (array_like, array_like)
         The labels corresponding to the datapoints in X. The length of y should
         match the first dimension of X. i.e., it should have length l.
-    folds : int, optional
-        The number of folds to use in m-fold cross validation.
+    folds : int, optional (default = None)
+        The number of folds to use in m-fold cross validation. If `None`,
+        cross-validation will not be performed.
+    metric : string, optional (default = 'euclidean')
+        The distance metric to use.
+    weights : string or callable, optional (default = 'uniform')
+        The weighting function to use.
 
     Returns
     -------
@@ -46,9 +111,10 @@ def k_iter(X, y, folds=None, metric='euclidean'):
     # pylint: disable=C0103
 
     errors = np.array([])
-    for k in tqdm(np.arange(1, 11), ):  # range is [1, 11)
+    for k in tqdm(np.arange(1, 11), desc=key):  # range is [1, 11)
         knn = KNeighborsClassifier(n_neighbors=k, metric=metric,
-                                   algorithm='brute', n_jobs=-1)
+                                   weights=weights, algorithm='auto',
+                                   n_jobs=-1)
         if folds is not None:
             kf = KFold(n_splits=folds)
             scores = np.array([])
@@ -84,6 +150,7 @@ def plot_results(errors, title="$k$NN: Error rate vs. $k$ neighbors",
     plt.legend()
     save_path = os.path.join(os.path.dirname(__file__), out)
     plt.savefig(save_path)
+    plt.show()
 
 
 def main():
@@ -96,14 +163,15 @@ def main():
     # pylint: disable=C0103
     # Problem 1
     data_path = os.path.join(os.path.dirname(__file__), 'data', 'iris.data')
-    data = pd.read_csv(data_path, header=None,
-                       names=["sepal-length", "sepal-width",
-                              "petal-length", "petal-width", "class"])
-    y = data.pop("class").values
+    data = pd.read_csv(data_path)
+    y = data.pop("species").values
     X = data.values
-    errors = {"5-fold cross validation": k_iter(X, y, folds=5),
-              "10-fold cross validation": k_iter(X, y, folds=10),
-              "Leave-one-out cross validation": k_iter(X, y, folds=len(y))}
+
+    keys = ["5-fold cross validation", "10-fold cross validation",
+            "Leave-one-out cross validation"]
+    errors = {keys[0]: k_iter(X, y, keys[0], folds=5),
+              keys[1]: k_iter(X, y, keys[1], folds=10),
+              keys[2]: k_iter(X, y, keys[2], folds=len(y))}
 
     title = "$k$NN, m-fold Cross Validation: Error rate vs. $k$ neighbors"
     plot_results(errors, title=title, out='p1.pdf')
@@ -122,13 +190,23 @@ def main():
     X = (X_train, X_test)
     y = (y_train, y_test)
 
-    errors = {"Cosine": k_iter(X, y, metric=cosine_distances),
-              "Euclidean": k_iter(X, y, metric='euclidean'),
-              "City-block": k_iter(X, y, metric='manhattan')
+    keys = ["Cosine", "Euclidean", "City-Block"]
+    errors = {keys[0]: k_iter(X, y, keys[0], metric='cosine'),
+              keys[1]: k_iter(X, y, keys[1], metric='euclidean'),
+              keys[2]: k_iter(X, y, keys[2], metric='manhattan')
               }
 
     title = "$k$NN Distance Metrics: Error rate vs. $k$ neighbors"
     plot_results(errors, title=title, out='p2.pdf')
+
+    # Problem 3
+    keys = ["Inverse", "Square Inverse", "Linear"]
+    errors = {keys[0]: k_iter(X, y, keys[0], weights=inverse_dist),
+              keys[1]: k_iter(X, y, keys[1], weights=square_inverse_dist),
+              keys[2]: k_iter(X, y, keys[2], weights=linear_dist)}
+
+    title = "k$NN Weighting Functions: Error rate vs. $k$ neighbors"
+    plot_results(errors, title=title, out='p3.pdf')
 
 
 if __name__ == "__main__":
